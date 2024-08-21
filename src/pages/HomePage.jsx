73bd0dc8970e830c357376fb5d1a555d64a1fc10
ethan.varaden <ethan.varaden@multichoice.co.za>
2024-08-21 11:41:43 +0200

    fixed home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,30 +1,23 @@
-import { Text, View } from '@lightningtv/solid';
-import { createSignal } from 'solid-js';
+import { Text, View } from "@lightningtv/solid";
+import { createSignal } from "solid-js";
 
-const ButtonStyle={
-  width:100,
-  height:100,
-  color:0x00ff00ff,
-  scale:1,
-  focus:{
-    scale:1.4,
-    color:0x0000ffff
-  }
-}
+const ButtonStyle = {
+  width: 100,
+  height: 100,
+  color: 0x00ff00ff,
+  scale: 1,
+  focus: {
+    scale: 1.4,
+    color: 0x0000ffff,
+  },
+};
 
 const HomePage = () => {
-
-  const [setText,getText] = createSignal('HELLO')
-  let left,right
   return (
     <View width={1920} height={1080} color={0xffff00ff}>
       <View x={700} y={400} width={400} height={400} color={0xff0000ff}>
-        <View x={50} y={200} style={ButtonStyle} autofocus onEnter={()=>{setText('BYE')}} onRight={()=>{right.setFocus()}} ref={left}>
-          <Text fontSize={20}>{getText()}</Text>
-        </View>
-        <View x={250} y={200} style={ButtonStyle} autofocus onEnter={()=>{setText('HI')}} onLeft={()=>{left.setFocus()}} ref={right}>
-          <Text fontSize={42}>{getText()}</Text>
-        </View>
+        <View x={50} y={200} style={ButtonStyle}></View>
+        <View x={250} y={200} style={ButtonStyle}></View>
       </View>
     </View>
   );
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,36 +1,38 @@
-import { test, expect } from '@playwright/test';
+import test from "@playwright/test";
+import { expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("has title", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  await page.screenshot({ path: 'screenshots/screenshot.png', fullPage: true });
+test("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+  await page.screenshot({ path: "screenshots/screenshot.png", fullPage: true });
   await expect(page).toHaveScreenshot({ maxDiffPixels: 100 });
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole("link", { name: "Get started" }).click();
 
   // Expects page to have a heading with the name of Installation.
   await expect(
-    page.getByRole('heading', { name: 'Installation' })
+    page.getByRole("heading", { name: "Installation" })
   ).toBeVisible();
 });
 
-test('go to home page', async ({ page }) => {
-  await page.goto('http://localhost:5173/#/');
+test("go to home page", async ({ page }) => {
+  await page.goto("http://localhost:5173/#/");
 });
 
-test('go to todo page', async ({ page }) => {
-  await page.goto('https://demo.playwright.dev/todomvc');
+test("go to todo page", async ({ page }) => {
+  await page.goto("https://demo.playwright.dev/todomvc");
+  await page.screenshot({ path: "screenshots/screenshot.png", fullPage: true });
+  // await expect(page).toHaveScreenshot({ maxDiffPixels: 10 });
+  const newTodo = page.getByPlaceholder("What needs to be done?");
+  await newTodo.fill("EAT ORANGE");
+  await newTodo.press("Enter");
   await expect(page).toHaveScreenshot({ maxDiffPixels: 10 });
-  const newTodo = page.getByPlaceholder('What needs to be done?');
-  await newTodo.fill('EAT ORANGE');
-  await newTodo.press('Enter');
-  await expect(page).toHaveScreenshot({ maxDiffPixels: 10 });
-  await expect(page.getByTestId('todo-title')).toHaveText(['EAT GRAPEFRUIT']);
+  await expect(page.getByTestId("todo-title")).toHaveText(["EAT GRAPEFRUIT"]);
 });
