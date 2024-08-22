226e06bd4e1cf4d7eea1c6978104478700ac94e2
EthanVaraden <Ethan.Varaden@multichoice.co.za>
2024-08-22 09:45:00 +0200

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -74,9 +74,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'pnpm start',
-    url: 'http://localhost:5173/#/',
-    reuseExistingServer: !process.env.CI,
-  },
+ // webServer: {
+   // command: 'pnpm start',
+    //url: 'http://localhost:5173/#/',
+    //reuseExistingServer: !process.env.CI,
+  //},
 });
